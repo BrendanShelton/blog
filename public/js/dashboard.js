c76e89bcdfc99d2b31393073da849beac53fb411
Brendan Shelton <brendan.d.shelton@gmail.com>
2023-02-15 14:26:48 -0500

    edit post functionality

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -38,12 +38,75 @@ const newFormHandler = async (event) => {
       }
     }
   };
+
+  const getContent = function (btn) {
+    // for collecting siblings
+    let siblings = []; 
+    // if no parent, return no sibling
+    
+    // first child of the parent node
+    let sibling  = btn.parentNode.firstChild;
+    
+    // collecting siblings
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== e) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+    return siblings[2];
+};
+
+  const editButtonHandler = async (event) => {
+    const btn = event.target
+    if (btn.hasAttribute('data-id')) {
+      const id = btn.getAttribute('data-id');
+
+      const parentEl = btn.parentElement;
+
+      const oldContent = parentEl.children[2].textContent;
+      
+      
+      editDiv = document.createElement('div')
+      console.log(parentEl.childNodes)
+      editDiv.innerHTML =`<textarea maxlength="5000" cols="60" rows="20">${oldContent}</textarea>` + '<button>submit</button>"'
+      parentEl.replaceChild(editDiv, parentEl.children[2])
+
+
+      //
+      /*editArea = document.createElement('textarea');
+      submitEditBtn = document.createElement('button');
+      editArea.textContent = oldContent
+      submitEditBtn.textContent = 'submit edit'
+      editDiv.appendChild(editArea);
+      editDiv.appendChild(submitEditBtn);*/
+      
+
+    
+    }
+  };
   
+  const submitEditHandler = async (event) => {
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to edit project');
+    }
+  }
   document
     .querySelector('#post-button')
     .addEventListener('click', newFormHandler);
 
-  btns = document.getElementsByClassName("delete-post");
-  for (const btn of btns) {
+  deleteBtns = document.getElementsByClassName("delete-post");
+  for (const btn of deleteBtns) {
       btn.addEventListener("click", delButtonHandler);
+    }
+
+  editBtns = document.getElementsByClassName("edit-post");
+  for (const btn of editBtns) {
+      btn.addEventListener("click", editButtonHandler);
     }
